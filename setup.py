0553d8c674cb36457f8e2ee6f560ee42f836ece8
csterling <chandlersterling1@gmail.com>
2015-07-27 21:16:17 -0700

    whatever

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='Morps',
-    version='0.1.8',
+    version='0.1.9',
     author='Chandler Sterling',
     author_email='chandlersterling1@gmail.com',
-    py_modules=['morps'],
+    packages=['morps'],
     url='http://pypi.python.org/pypi/morps/',
     license='LICENSE.txt',
     description='Modifying Objects to Reduce Python Scripting',
