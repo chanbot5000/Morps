43bdb3972a8d36efe4b1f82167a35fad47a85f9d
csterling <chandlersterling1@gmail.com>
2015-07-27 21:17:25 -0700

    Revert ff561f8..0553d8c
    This rolls back to commit ff561f839a42cffb884b75151312212520b0aeb2.

--- morps.py
+++ morps.py
@@ -11,9 +11,6 @@ driverList = {"shp":"ESRI Shapefile","json":"GeoJSON","kml":"KML"}
 ### Create unit converter function
 ### Create feature count function
 ### Retain attribute information when creating new files
-def test():
-    return "this is a test and it is working"
-
 
 def unitConverter():
 
